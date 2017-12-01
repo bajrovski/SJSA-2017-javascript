2868ecee8f86cd7e49c9c89df400e9fbac00bd14
Emil Bajrovski <bajrovski@gmail.com>
2017-12-01 20:54:38 +0100

    vtor cas

---
+++ 02/js/global.js
@@ -0,0 +1,64 @@
+// var myFirstObject = {firstName: "Emil", lastName: "Bajrovski", favoriteAuthor: "David Foster Wallace"};
+// console.log(myFirstObject);
+// console.log (myFirstObject.favoriteAuthor);
+// console.log (myFirstObject.firstName);
+
+// console.log ("Jas sum " + myFirstObject.firstName + " " + myFirstObject.lastName + ". " + "Mojot omilen avtor e " + myFirstObject.favoriteAuthor )
+
+// var yearsScale = {5: "Baby", 100: "Old"};
+// console.log (yearsScale);
+// console.log (yearsScale["5"]);
+
+// var person = {name: "Kobe"};
+// var anotherPerson = person;
+
+// person = "Bryant";
+// console.log(anotherPerson.name);
+// console.log (person.name);
+
+// var mango = new Object();
+// mango.color = "yellow";
+// mango.shape = "round";
+// mango.sweetnesslevel = 8;
+
+// console.log(mango);
+// console.log(mango.sweetnesslevel);
+
+// delete mango.sweetnesslevel;
+// console.log(mango);
+
+// var book = {
+// 	name: "Infinite Jest",
+// 	year: 1994,
+// 	author: "David Foster Wallace",
+// 	genre: "Satire",
+
+// }
+
+// console.log(book.name, book.year, book.author, book.genre);
+
+// for(var x in book){
+// 	console.log(book[x]);
+// }
+
+// for(var i = 0; i <= 10; i+){
+// 	console.log("Current count is " + i);
+// }
+
+// for(var i = 100; i >= 0; i-=10){
+// 	console.log("Current count is " + i);
+// }
+
+// var names = ["mario", "dimitar", "david", "jovan", "cacko", "dragan", "tomas", "jordan", "drake", "sashko", "emil"]
+
+// for(var i = 0; i < names.length; i++){
+// 	if(names[i].length == 5){
+// 		console.log(names[i]);
+// 	}
+// }
+
+var i = 0;
+while (i <= 5){
+	console.log("Current count is " + i);
+	i++;
+}
